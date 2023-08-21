14a10b19e0f420fa34b54f478ca1e23f94b043e6
Mohamed Yusuf <mohamed208.y@proton.me>
2023-08-21 09:31:19 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
+    def sit(self):
+        print('The dog is sitting.')
 
+    def bark(self):
+        print('Woof!')
     #Instance method definition
+    def walk(self):
+        print('The person is walking.')
     pass
+
+
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,13 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print('Hello World!')
+        
+    def sit(self):
+        print('The dog is sitting.')
+    #Instance method definition
+    def walk(self):
+        print('The person is walking.')
     #Instance method definition
     pass
